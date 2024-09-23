d31729859edfed675be10249d008e70b3e18f945
DylanFlashman <DFlashman17@gmail.com>
2024-09-23 15:10:55 +0200

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
+import {Route,Routes} from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import PostList from "./components/postList";
+import EditPost from "./components/postEdit";
+import CreatePost from "./components/postCreate";
+import Register from "./components/register";
+import Login from "./components/login";
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route exact path ="/" element = {<PostList/>}/>
+        <Route exact path ="/edit/:id" element = {<EditPost/>}/>
+        <Route exact path ="/create" element = {<CreatePost/>}/>
+        <Route exact path ="/register" element = {<Register/>}/>
+        <Route exact path ="/login" element = {<Login/>}/>
+      </Routes>
     </div>
   );
 }
